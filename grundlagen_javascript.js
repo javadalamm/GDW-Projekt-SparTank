ed8c5685f79fe7b0fb92352f19e7a304c8d5d490
TugceAtay <56542136+TugceAtay@users.noreply.github.com>
2019-10-28 12:21:50 +0100

    Add files via upload

---
+++ grundlagen_javascript.js
@@ -0,0 +1,46 @@
+//Aufgabe 1)
+
+// Ausgabe auf der Konsole
+console.log("Aufgabe 1.");
+console.log("");
+console.log("Ich heiße Tugce");
+
+Console.log(""); // Leere Zeile für Gute Übersicht
+
+//Aufgabe 2)
+console.log("Aufgabe 2.");
+console.log("");
+
+const max_hoehe = 5;
+var aktuelleAnzahlBewertungen = 50; // Teilnahme der Personen
+var gespeicherteBewertungen = 250; // alle geben 5 Sterne (50*5)
+
+console.log("Dies ist eine Bewertung");
+console.log("Maximale Sterne:" , max_hoehe);
+console.log("Anzahl der Bewertung:" , aktuelleAnzahlBewertungen);
+console.log("Sterne insgesamt:" , gespeicherteBewertungen);
+console.log("");
+
+// Zusätzliche Bewertung / Veränderte Werte
+// mit lt oder const (-> zuweisen eines anderen Datentyps) 
+
+console.log("Es kommt eine neue Bewertung dazu. Die Werte ändern sich");
+
+// ändern der Werte
+aktuelleAnzahlBewertungen = 51 // eine neue Bewertung 
+gespeicherteBewertungen = 251 // nur ein Stern Bewertung 
+
+console.log("Anzahl der aktuellen Bewertungen:" , aktuelleAnzahlBewertungen);
+console.log("Sterne insgesamt:" , gespeicherteBewertungen);
+console.log("");
+
+/* Was macht Javascript, wenn Sie eine der Variablen  einen anderen Typ zuweisen?
+... */
+
+
+//Aufgabe 3)
+console.log("Aufgabe 3.");
+
+
+
+
